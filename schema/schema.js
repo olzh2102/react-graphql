efb0736894810fbc371d2aac144a67de456906c9
Olzhas Kurikov <olzhas2102@yahoo.com>
2020-02-25 18:06:03 +0100

    changed string type into graphqlid

--- schema/schema.js
+++ schema/schema.js
@@ -1,11 +1,18 @@
 const graphql = require('graphql')
 
-const {GraphQLObjectType, GraphQLString, GraphQLSchema} = graphql
+const {GraphQLObjectType, GraphQLString, GraphQLSchema, GraphQLID} = graphql
+
+const movies = [
+    {id: 1, name: 'About Time', genre: 'Drama'},
+    {id: 2, name: 'The Gentlemen', genre: 'Gansta'},
+    {id: 3, name: 'Ford vs Ferrari', genre: 'Sport'},
+    {id: 4, name: 'Aviator', genre: 'Science'}
+]
 
 const MovieType = new GraphQLObjectType({
     name: 'Movie',
     fields: () => ({
-        id: {type: GraphQLString},
+        id: {type: GraphQLID},
         name: {type: GraphQLString},
         genre: {type: GraphQLString}
     })
@@ -16,9 +23,9 @@ const Query = new GraphQLObjectType({
     fields: {
         movie: {
             type: MovieType,
-            args: {id: {type: GraphQLString}},
+            args: {id: {type: GraphQLID}},
             resolve(parent, args) {
-    
+                return movies.find(movie => movie.id == args.id)
             }
         }
     }
